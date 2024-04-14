efdedd98d9ea2c01b89b8628e7515ccc8cb1e44b
MatheusPalmieri <matheuspalmieridev@gmail.com>
2024-04-14 05:19:40 -0300

    feat: navbar navigate

--- src/components/Navbar/Link.tsx
+++ src/components/Navbar/Link.tsx
@@ -1,6 +1,19 @@
-export const NavbarLink = ({ children }: { children: string }) => {
+import { useNavigate } from "react-router-dom";
+
+export const NavbarLink = ({
+  children,
+  route,
+}: {
+  children: string;
+  route: string;
+}) => {
+  const navigate = useNavigate();
+
   return (
-    <li className="text-zinc-200 text-lg font-medium hover:text-white cursor-pointer duration-300">
+    <li
+      className="text-zinc-200 text-lg font-medium hover:text-white cursor-pointer duration-300"
+      onClick={() => navigate(route)}
+    >
       {children}
     </li>
   );
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import { NavbarLink } from "./Link";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="p-6">
       <nav
@@ -9,13 +12,20 @@ export const Navbar = () => {
         flex items-center justify-between
       "
       >
-        <h1 className="text-2xl font-medium">Matheus Palmieri</h1>
+        <h1
+          className="text-2xl font-medium cursor-pointer"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Matheus Palmieri
+        </h1>
 
         <ul className="flex gap-3">
-          <NavbarLink>Home</NavbarLink>
-          <NavbarLink>About me</NavbarLink>
-          <NavbarLink>Skills</NavbarLink>
-          <NavbarLink>Projects</NavbarLink>
+          <NavbarLink route="/">Home</NavbarLink>
+          <NavbarLink route="/about">About me</NavbarLink>
+          <NavbarLink route="/skills">Skills</NavbarLink>
+          <NavbarLink route="/projects">Projects</NavbarLink>
         </ul>
       </nav>
     </div>
