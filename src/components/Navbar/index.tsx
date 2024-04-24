e6a798ca2ad9e0d3983fc12145f6ba8aa06e4beb
MatheusPalmieri <matheuspalmieridev@gmail.com>
2024-04-24 12:14:09 -0300

    feat: routes

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -18,15 +18,9 @@ export const Navbar = () => (
 
       <ul className="flex gap-3">
         <NavbarLink route="/">Home</NavbarLink>
-        <NavbarLink route="/about" disabled>
-          About me
-        </NavbarLink>
-        <NavbarLink route="/skills" disabled>
-          Skills
-        </NavbarLink>
-        <NavbarLink route="/projects" disabled>
-          Projects
-        </NavbarLink>
+        <NavbarLink route="/about">About me</NavbarLink>
+        <NavbarLink route="/skills">Skills</NavbarLink>
+        <NavbarLink route="/projects">Projects</NavbarLink>
       </ul>
     </nav>
   </div>
